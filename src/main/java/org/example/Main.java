243b742068a51c4f2b2d7387e2e53641d48b1df6
Aleksey Galishchev <galishchev@inbox.ru>
2023-04-17 20:20:34 +0300

    Adapter add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.sum(10, 22));
+        System.out.println(intsCalc.mult(5, 5));
+        System.out.println(intsCalc.pow(2, 10));
     }
 }
